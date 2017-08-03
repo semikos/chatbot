678c1e87b8f8ff3deba4ebf246090d5b72d12763
Noujoud <b.noujoud95@gmail.com>
2017-08-03 14:51:11 +0100

    message2

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ app.post('/webhook/', function (req, res) {
 			 console.log('message sent')
             continue
         }
+		
+		/*********************************************************************************************************************************/
+		 if (text === 'Menu') {
+            sendMenuMessage(sender, "!! " +event, token)
+			 console.log('message sent')
+            continue
+        }
+		/*********************************************************************************************************************************/
+		
         console.log('message sent before sendApiMessage')
 		sendApiMessage(event)
 		console.log('message sent after sendApiMessage')
@@ -132,6 +141,56 @@ function sendGenericMessage(sender) {
     })
 }
 
+/*******************************************************************************************************************************/
+function sendMenuMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "menu",
+                "elements": [{
+                    "title": "4 exemples de Chatbots",
+                    "subtitle": "Chatbot 1 : Adecco France",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://m.me/adecco.france",
+                        "title": "Accés"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "4 exemples de Chatbots",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+/*****************************************************************************************************************************/
 
 //Send message using API.AI
 function sendApiMessage(event) {
