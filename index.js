e1bed8be085261a26ce8d48def6c21295a47b27b
souhaibBelguith <souhaib.belguith@esprit.tn>
2017-08-07 11:50:40 +0100

    message2

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/webhook/', function (req, res) {
 	  
       if (event.message && event.message.text) {
 		  console.log('eventmessage')
-        let text = event.message.text
+        let text = event.message.text 
         if (text === 'Generic') {
             sendGenericMessage(sender, "!! " +event, token)
 			 console.log('message sent')
@@ -53,7 +53,7 @@ app.post('/webhook/', function (req, res) {
 		
 		/*********************************************************************************************************************************/
 		else if (text.toUpperCase() === 'Menu'.toUpperCase() || (text.toUpperCase().indexOf('nouveau'.toUpperCase()) !== -1)
-			|| (text.toUpperCase().indexOf('neuf'.toUpperCase()) !== -1)) {
+			|| (text.toUpperCase().indexOf('neuf'.toUpperCase()) !== -1) || (text.toUpperCase().indexOf('bot'.toUpperCase()) !== -1) {
             sendMenuMessage(sender, "!! " +event, token)
 			 console.log('message sent')
             continue
