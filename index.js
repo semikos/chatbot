247ffbc735fb6b8405099a14c052c5255b33fe30
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-08 18:25:57 +0100

    message2

--- index.js
+++ index.js
@@ -98,9 +98,25 @@ function processPostback(event) {
         greeting = "Hi " + name + ". ";
       }
       
-      sendTextMessage(senderId, {text: greeting});
+      sendMessage(senderId, {text: greeting});
     });
   }
+}
+
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: token},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
 }
 
 function sendTextMessage(sender, text) {
