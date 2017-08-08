baedc286ffd97986a63029f409cbd0fd0c482b76
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-08 18:21:06 +0100

    message2

--- index.js
+++ index.js
@@ -67,13 +67,41 @@ app.post('/webhook/', function (req, res) {
 			sendApiMessage(event)
 		}
 		else if (event.postback) {
-			Demarrer();
+			processPostback(event)
 			continue
 		}
     }
     res.sendStatus(200)
 })
 
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: token,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
