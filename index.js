049ae83d20ad1eb32239aa15da8bd4cdd300aca4
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-08 18:24:39 +0100

    message2

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ function processPostback(event) {
         name = bodyObj.first_name;
         greeting = "Hi " + name + ". ";
       }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
+      
+      sendTextMessage(senderId, {text: greeting});
     });
   }
 }
