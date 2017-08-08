f6f52cc8cfdc78bd3477f6fda89f48ffa3685928
souhaibBelguith <souhaib.belguith@esprit.tn>
2017-08-08 11:49:15 +0100

    message2

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ facebookDemarre();
 facebookMenu();
 
 app.post('/webhook/', function (req, res) {
+	
+	sendButtonMessage(sender,event,token)
+			 console.log('message sent')
+            continue
+			
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
