0eb6d51d3ff40b147f91169a0c0955ed04c11047
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-10 13:00:19 +0100

    message2

--- index.js
+++ index.js
@@ -57,11 +57,8 @@ mongo.connect(url, function (err,db) {
 	});
 });
 
-facebookDemarre();
-
 // Posting to the webhook and Facebook messenger application.
 app.post('/webhook/', function (req, res) {
-	facebookMenu();
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
