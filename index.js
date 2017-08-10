65cf405fb2f291a38283cf2b0dde319b1a643207
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-10 17:35:54 +0100

    message2

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ discussionButtons();
 // Posting to the webhook and Facebook messenger application.
 app.post('/webhook/', function (req, res) {
 	facebookMenu();
+	discussionButtons(event);
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
