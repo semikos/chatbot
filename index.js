f43974f4973bc19aaf16f9ac4f8a4c8c3878c0f6
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-11 11:41:53 +0100

    message2

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
-var url = "mongodb://localhost:27017/test";
+var url = "mongodb://chatbotcybex:chatbotcybex123@bot-shard-00-00-ccjjw.mongodb.net:27017,bot-shard-00-01-ccjjw.mongodb.net:27017,bot-shard-00-02-ccjjw.mongodb.net:27017/bots?ssl=true&replicaSet=Bot-shard-0&authSource=admin";
 
 var item = {
 	name : "Adecco",
@@ -50,11 +50,7 @@ var item = {
 
 mongo.connect(url, function (err,db) {
 	assert.equal(null, err);
-	db.collection('bot-data').insertOne(item, function(err, result) {
-		assert.equal(null, err);
-		console.log(item);
-		db.close();
-	});
+	console.log(item);
 });
 
 facebookDemarre();
