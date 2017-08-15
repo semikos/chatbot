a1f821ba095642d8b68329e12096da7ddf238da7
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-15 11:15:03 +0100

    message2

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post('/webhook/', function (req, res) {
 			sendApiMessage(event)
 		}
 		if (event.postback && event.postback.payload) {
-			sendTextMessage(sender, event.postback.payload, token);
+			sendTextMessage(sender, event.postback.payload+"  "+getUserInfos, token);
 			discussionButtons(sender);
 			continue
 		}
