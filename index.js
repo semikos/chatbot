03071b5fe7dce26c9daea113b53039a270c20c39
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-17 16:06:27 +0100

    message2

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ var facebook = require('./facebook.js')
 
 app.set('port', (process.env.PORT || 5000))
 
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -28,11 +33,6 @@ app.get('/webhook/', facebook.VerificationToken)
 
 // Posting to the webhook and Facebook messenger application.
 app.post('/webhook/', facebook.postMessages)
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
 
 facebook.facebookDemarre();
 facebook.facebookMenu();
