e768245eb3dfc87a73443a32d4ffec3606f5200f
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-24 16:40:51 +0100

    message2

--- mongo.js
+++ mongo.js
@@ -23,7 +23,7 @@ app.get('/get-data', function(req, res, next) {
 	var resultArray = [];
 	mongo.connect(url, function(err,db) {
 		assert.equal(null, err);
-		var cursor = db.collection('bot-data').find();
+		var cursor = db.collection('user-data').find();
 		cursor.forEach(function(doc, err) {
 			assert.equal(null, err);
 			resultArray.push(doc);
