a086d0c45f1206877f89b37ebd9bc8cc3f01c865
Mahmoud <mahmoud.bouden@esprit.tn>
2017-08-30 15:07:54 +0100

    message2

--- facebook.js
+++ facebook.js
@@ -10,7 +10,6 @@ const app = express()
 const mongo = require('mongodb').MongoClient;
 const mongoJS = require('./mongo.js');
 const assert = require('assert')
-const schedule = require('node-schedule')
 const classe = require('./User.js');
 
 var url = "mongodb://chatbotcybex:chatbotcybex123@bot-shard-00-00-ccjjw.mongodb.net:27017,bot-shard-00-01-ccjjw.mongodb.net:27017,bot-shard-00-02-ccjjw.mongodb.net:27017/bots?ssl=true&replicaSet=Bot-shard-0&authSource=admin";
@@ -28,25 +27,7 @@ function VerificationToken(req, res) {
 }
 
 function postMessages (req, res) {
-	//Scheduled Messages
-	var rule = new schedule.RecurrenceRule();
-	rule.dayOfWeek = 1;
-	rule.hour = 8;
-	rule.minute = 0;
-	var j = schedule.scheduleJob(rule, function(){
-		var resultArray = [];
-		mongo.connect(url, function(err,db) {
-			assert.equal(null, err);
-			var cursor = db.collection('user-data').find();
-			cursor.forEach(function(err, doc) {
-				//sendTextMessage(doc['id'], "Hello" ,token);
-				console.log(doc+ "  Hello");
-			}, function (){
-				
-			})
-		})
-	});
-	// \\\\\\\\\\\\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\\\\\\\\\\\\ //
+	
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 		var event = req.body.entry[0].messaging[i]
